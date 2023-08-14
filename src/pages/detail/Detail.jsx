eba3d21ab422aedf79dd30a33232e1ec46423fc0
fulya_ture <fulyature@gmail.com>
2023-08-14 18:59:54 +0300

    style:Card Container

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import {
+  Description,
+  DetailContainer,
+  DetailImg,
+  DetailPart,
+  DetailTitle,
+  InfoPart,
+} from "./Detail.style";
 
 const Detail = () => {
+  const { state } = useLocation();
+  console.log(state);
   return (
-    <div>Detail</div>
-  )
-}
+    <DetailContainer>
+      <DetailPart>
+        <DetailTitle></DetailTitle>
+        <DetailImg>
+          <img src="" alt="" />
+        </DetailImg>
+        <Description></Description>
+        <InfoPart></InfoPart>
+      </DetailPart>
+    </DetailContainer>
+  );
+};
 
-export default Detail
+export default Detail;
